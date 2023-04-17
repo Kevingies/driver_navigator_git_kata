babc5f581486d6cc20d16cc7188bcc22e95160fe
Richard Sihombing <richardsihombing@Richards-MacBook-Air.local>
2023-04-17 15:47:06 +0200

    only the utils.py from Navigator side

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
 def pulltitanic():
-    df = pd.read_csv("/Users/kevingiesen/Desktop/venv/driver_navigator_git_kata/data/titanic.csv")
-    df['Sex'] = "Female"
+    df = pd.read_csv("/Users/richardsihombing/driver_navigator_git_kata/data/titanic.csv")
+    df['Sex'] = "male"
     return df               
